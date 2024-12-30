f7ec6e932b20d000dcb369853ce35851e5ef888b
den4ik <denis.shatalov95@gmail.com>
2024-12-31 01:25:18 +0200

    hw-js9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
-console.log("form")
+const formData = {
+  email: "",
+  message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const STORAGE_KEY = "feedback-form-state";
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+}
+
+form.addEventListener('input', (event) => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = "";
+  formData.message = "";
+
+  form.reset();
+});
